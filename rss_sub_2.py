808d560437d4e5ea175043d3964cef4c4b8a25b4
Anton Masyan <anton@masyan.ru>
2018-07-05 14:05:57 +0300

    add telegram py file

---
+++ rss_sub_2.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import configparser
+from datetime import datetime, timedelta
+from time import mktime, strftime
+import telebot
+import feedparser
+import pickle
+
+"""
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+"""
+
+# создание бота с его токеном API и подгружаем конфиг
+config = configparser.ConfigParser()
+config.read("config.ini")
+bot = telebot.TeleBot(config["Telegram"]["access_token"])
+
+
+def auto_posting():
+    bot.send_message('-179710499', 'Test')
+
+f = open("sccm_rss.txt")
+RSS_URLS = f.readlines()
+
+feeds = []
+for url in RSS_URLS:
+    feeds.append(feedparser.parse(url))
+
+rss = []
+lastHourDateTime = datetime.today() - timedelta(hours=48)
+
+for feed in feeds:
+    try:
+        title = feed['entries'][0].title
+        url = feed['entries'][0].link
+        date_p = feed['entries'][0].updated_parsed
+
+        # convert from struct_date to str
+        # t = strftime("%Y-%m-%d %H:%M:%S", date_p)
+
+        # convert struct date to datetime.datetime
+        t = datetime.fromtimestamp(mktime(date_p))
+
+        if t > lastHourDateTime:
+            rss.append(title + ' ' + url + '\n\n')
+
+    except IndexError:
+        pass
+
+print('лента', rss)
+
+if rss is None:
+    # sccm channel
+    # bot.send_message('-1001054149356', "Сегодня новостей нет! ;( ")
+    # test channel
+    bot.send_message('-179710499', "Сегодня новостей нет! ;( ")
+else:
+    # sccm channel
+    # bot.send_message('-1001054149356', "Configuration Manager news: \n\n" + "".join(rss), parse_mode="Markdown",disable_web_page_preview=True)
+    # test channel
+    with open('rss_sccm.txt', 'w') as f:
+        f.write(str(rss))
+
+    bot.send_message('-179710499', "Configuration Manager news: \n\n" + "".join(rss), parse_mode="Markdown", disable_web_page_preview=True)
+    f.close()
+    # bot.send_message('-179710499', title + ' ' + url, disable_web_page_preview=True)
